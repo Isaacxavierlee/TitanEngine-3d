a5d1afaa9ee3cbcb6d828732887bbaed22dba965
Isaacxavierlee <reivax.caasi@gmail.com>
2026-04-11 12:33:17 +0800

    Add project files.

---
+++ TitanEngine/TitanEngine.cpp
@@ -0,0 +1,12 @@
+﻿// TitanEngine.cpp : Defines the entry point for the application.
+//
+
+#include "TitanEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
